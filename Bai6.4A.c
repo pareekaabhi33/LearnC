1c88d8153797ac669dd309b48efb5bd9647ae76c
pareekaabhi33 <72213935+pareekaabhi33@users.noreply.github.com>
2020-10-01 21:35:45 +0530

    Update Bai6.4A.c

--- Bai6.4A.c
+++ Bai6.4A.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 struct HoTen {
-	char ho[20];
-	char dem[20];
-	char ten[20];
+	char a[20];
+	char b[20];
+	char c[20];
 };
 
 struct DiemMH {
-	float toan;
-	float van;
-	float anh;
-	float tbc;
+	float d;
+	float e;
+	float f;
+	float g;
 };
 
 struct SinhVien {
-	int ma;
+	int h;
 	struct HoTen hoVaTen;
-	int tuoi;
-	char gioiTinh[10];
+	int i;
+	char j[10];
 	struct DiemMH diem;
 };
 
